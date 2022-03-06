b663cc40eba504c68e5e8f43df97fcb43fc8e233
Impatch <edgarbarboza@Natalys-MacBook-Pro.local>
2022-03-06 10:19:44 -0800

    commit

---
+++ problems/CourseSchedule/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+       return Solution.canFinish(numCourses, prerequisites); 
+    }
+
+    private static class Solution1 {
+        static boolean canFinish(int numCourses, int[][] prerequisites) {
+            return false;
+        }
+
+        Solution1() {}
+    }
+}
