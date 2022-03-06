14dac743b705b160cf9f1a6d49396b935f5683a9
Impatch <edgarbarboza@Natalys-MacBook-Pro.local>
2022-03-06 00:18:52 -0800

    Yet another problem

--- problems/CloneGraph/Solution.java
+++ problems/CloneGraph/Solution.java
@@ -20,6 +20,37 @@ class Node {
 
 class Solution {
     public Node cloneGraph(Node node) {
-        
+        return Solution1.cloneGraph(node);
+    }
+
+    private static class Solution1 {
+        /*
+         * TimeComplexity:  O(N)
+         * SpaceComplexity: O(N)
+         */
+        Map<Integer,Node> seen;
+
+        Node copy(Node node) {
+            if (node == null) {
+                return null;
+            }
+            if (seen.containsKey(node.val)) {
+                return seen.get(node.val);
+            }
+            Node newNode = new Node(node.val, new ArrayList());
+            seen.put(node.val, newNode);
+            for (Node neighbor : node.neighbors) {
+                newNode.neighbors.add(copy(neighbor));
+            }
+            return newNode;
+        }
+
+        static Node cloneGraph(Node node) {
+            return new Solution1().copy(node);
+        }
+
+        private Solution1() {
+            seen = new HashMap<>();
+        }
     }
 }
