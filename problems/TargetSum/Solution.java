f9a5463478638a93c78dc9cdd67890763c2c160d
Impatch <edgarbarboza@Natalys-MacBook-Pro.local>
2022-03-06 12:57:16 -0800

    new problem

---
+++ problems/TargetSum/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
+        return Solution1.findTargetSumWays(nums, target);
+    }
+
+    private static class Solution1 {
+        static int findTargetSumWays(int[] nums, int target) {
+            // throw new RuntimeException("Unimplemented");
+            return -1;
+        }
+
+        Solution1() {}
+    }
+}
