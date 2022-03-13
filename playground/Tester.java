1e2782fff5f0c5f4e438566afb81ad87ffff65a1
Impatch <edgarbarboza@Natalys-MacBook-Pro.local>
2022-03-12 20:20:49 -0800

    temp

---
+++ playground/Tester.java
@@ -0,0 +1,8 @@
+package playground;
+
+public class Tester {
+     public static void main(String[] unusedArgs) {
+         System.out.println("Hello, world!");
+         Solution solution = new Solution();
+     }
+}
--- playground_setup/playground_builder.py
+++ playground_setup/playground_builder.py
@@ -4,19 +4,21 @@ from logger import Logger
 
 import sys
 
-
 class PlaygroundBuilder(object):
+    '''
+    Generates:
+                    ~
+                    |
+                  GitHub
+                    |
+                playground
+                ... | ...
+            <LeetCode_Problem>
+             |              |
+        Solution.java   Tester.java
+    '''
 
     LOGGING_PREFIX = 'PlaygroundBuilder'
-
-    TESTER_CONTENT = (
-            'package playground;\n'
-            '\n'
-            'public class Tester {\n'
-            '     public static void main(String[] unusedArgs) {\n'
-            '         System.out.println(\"Hello, world!\");\n'
-            '     }\n'
-            '}')
 
     def __init__(self, exercise=None):
         assert type(exercise) is Exercise
@@ -26,10 +28,13 @@ class PlaygroundBuilder(object):
         self.playground_dir = (
                 f'{self.file_utils.get_home_dir()}/'
                 'GitHub/playground')
-        self.tester_file = (
-                f'{self.playground_dir}/Tester.java')
-        self.solution_file = (
+        self.title_dir = (
                 f'{self.playground_dir}/'
+                f'{self.exercise.get_title().toLowerCase()}')
+        self.tester_file = (
+                f'{self.title_dir}/Tester.java')
+        self.solution_file = (
+                f'{self.title_dir}/'
                 f'{self.exercise.get_title()}.java')
 
     def log(self, msg=None):
@@ -38,21 +43,26 @@ class PlaygroundBuilder(object):
 
     def build_playground(self):
         try:
-            # Create playground (if it doesn't exist).
+            # Create playground dir (if it doesn't exist).
             if not self.file_utils.is_dir(self.playground_dir):
                 self.log('Creating dir: '
                          f'{self.playground_dir}')
                 self.file_utils.create_dir(self.playground_dir)
+            # Create title dir (if it doesn't exist).
+            if not self.file_utils.is_dir(self.title_dir):
+                self.log('Creating dir: '
+                         f'{self.title_dir}')
+                self.file_utils.create_dir(self.title_dir)
             # Create/clear Tester.java.
             self.log('Creating (or clearing) file: '
                      f'{self.tester_file}')
             self.file_utils.create_file(self.tester_file)
             # Fill Tester.java with base info.
-            self.log('Appending PlaygroundBuilder.TESTER_CONTENT to: '
+            self.log('Appending self.get_tester_file_content() to: '
                      f'{self.tester_file}')
             self.file_utils.append_to_file(
                     self.tester_file,
-                    PlaygroundBuilder.TESTER_CONTENT)
+                    self.get_tester_file_content())
             # Copy LeetCode problem to playground dir.
             self.log('Copying:\n'
                      f'src={self.exercise.get_exercise()}\n'
@@ -62,4 +72,14 @@ class PlaygroundBuilder(object):
                     dst=self.solution_file)
         except Exception as e:
             self.log(f'Failed to build playground: {e}')
-            sys.exit()   
+            sys.exit()
+
+    def get_tester_file_content(self):
+        return (
+            f'package playground.{self.exercise.get_title().toLowerCase()};\n'
+            '\n'
+            'public class Tester {\n'
+            '     public static void main(String[] unusedArgs) {\n'
+            '         System.out.println(\"Hello, world!\");\n'
+            '     }\n'
+            '}');
