f7e97ae80ef3b57a0ff6105d140e944d8bdfcc9b
Impatch <edgarbarboza@Natalys-MacBook-Pro.local>
2022-03-18 22:27:25 -0700

    update

--- playground/expressionaddoperators/Solution.java
+++ playground/expressionaddoperators/Solution.java
@@ -5,126 +5,244 @@ import java.util.List;
 import java.lang.String;
 import java.lang.Character;
 
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptEngine;
+import javax.script.ScriptException;
+
+
 class Solution {
+    private final static ScriptEngine SCRIPT_ENGINE =
+        new ScriptEngineManager().getEngineByName("JavaScript");
+
+    private static int evalExpr(String expr) {
+        try {
+            return (Integer) Solution.SCRIPT_ENGINE.eval(expr);
+        } catch (ScriptException e) {
+            throw new RuntimeException(String.format(
+                        "Script Engine Error with expr=\"%s\"\n%s",
+                        expr, e));
+        }
+    }
+
     public List<String> addOperators(String num, int target) {
         return Solution1.addOperators(num, target);
     }
 
-    private static class Solution1 {
-        // Naive (1st attempt) implementation.
-        // Possible operators: {+, -, *}.
-        // Say you have the sequence (string) of digits:
-        //      d_0, d_1, ..., d_(n-1).
-        // Then you only assign an operator to all except the first digit (d_0).
-        //
-        // at any given time (ie, when you are looking at digit d_i), you have a
-        // current_sum and a current_factor.
-        // What options do we have with d_i ?
-        //
-        //   PLUS(+):
-        //      (current_sum + current_factor, + d_i)
-        //      --> Rationale: as soon as you assign a non-multiplier op, you
-        //          "break" the factor stream (and thus it "ends" -- must be
-        //          added to current_sum).
-        //  MINUS(-):
-        //      (current_sum + current_factor, - d_i)
-        //      --> Rationale: Same as the PLUS rationale.
-        //  MULTIPLY(*):
-        //      (current_sum, current_factor * d_i)
-        //      --> Rationale: You are extending the multiplication stream.
-        //
-        // At each step, you consume the digit (ie, increment the index).
-        //
-        // At d_0, there is no concept of (current_sum, current_factor).
-        // Well, actually, there is: (0, 1)
-        // However, you cannot assign the MINUS(-) operator at d_0.
-        // So at d_0, you can only generate: {(d_0, 1), (0, +d_0)}
-        //
-        // At index N, there are no more digits, which indicates that you must
-        // consolidate the existing tuple:
-        //      (current_sum, current_factor) --> current_sum + current_factor
-        // Then, compare this value with `target`:
-        //      if (i == N && current_sum + current_factor == target) {
-        //          possibilities.add(current_sting);
-        //          return;
-        //      }
-        //
-        // We've been ignoring the construction of the final string:
-        //      OP: (current_sting + "${OP}" + d_i), for OP in {+, -, *}.
-        //
+    static class Eval {
+        private final int value;
+        private final String stringToAppend;
 
-        private final String num;
-        private final int target;
-        private final List<String> possibilities;
-
-        static List<String> addOperators(String num, int target) {
-            return new Solution1(num, target).generatePossibilities();
+        Eval(int value, String stringToAppend) {
+            this.value = value;
+            this.stringToAppend = stringToAppend;
         }
 
-        List<String> generatePossibilities() {
-            // Initial states:
-            int initialIndex = 0; 
-            char digitAsChar = getDigitAsChar(0);
-            int digitAsInt = getDigitAsInt(0);
+        static Eval create(int value, String stringToAppend) {
+            return new Eval(value, stringToAppend);
+        }
+
+        int getValue() {
+            return value;
+        }
+
+        String getStringToAppend() {
+            return stringToAppend;
+        }
+    }
+
+    private static class Solution1 {
+        // Naive (1st attempt) implementation.
+
+        private final String num;
+        private final List<String> possibilities;
+
+        enum Operation { ADDITION, SUBTRACTION, MULTIPLICATION, APPEND};
+
+        static List<String> addOperators(String num, int target) {
+            return new Solution1(num).generatePossibilities(target);
+        }
+
+        List<String> generatePossibilities(int target) {
+
+            if (num.length() == 0) {
+                return possibilities;
+            }
+            if (num.length() == 1) {
+                if (getDigitAsInt(0) == target) {
+                    possibilities.add(num);
+                }
+                return possibilities;
+            }
 
             generatePossibilities(
+                    /* target= */ target,
+                    /* x= */ 0,
+                    /* y= */ getDigitAsInt(0),
+                    /* op= */ Operation.ADDITION,
                     /* index= */ 1,
-                    /* sum= */ 0,
-                    /* factor= */ digitAsInt,
-                    /* possibility= */ Character.toString(digitAsChar));
+                    /* possibility= */ String.format("%d", getDigitAsInt(0)));
 
             return possibilities;
         }
 
+        /*
+         * Appends num[index] to y and preserves previous operation (since APPEND is not mathematical).
+         */
+        void doAppendOp(
+                int target, int x, int y, Operation op, int index, String possibility) {
+            generatePossibilities(
+                    /* target= */ target,
+                    /* x= */ x,
+                    /* y= */ eval(y, getDigitAsInt(index), Operation.APPEND),
+                    /* op= */ op,
+                    /* index= */ index + 1,
+                    /* possibility= */ String.format("%s%d", possibility, getDigitAsInt(index)));
+        }
+
+        /*
+         * num[index] will be preceded with a + operation. Since this operator has low precedence,
+         * we can apply previous operator first (hence consolidate (x, y) into x_prime).
+         */
+        void doAdditionOp(
+                int target, int x, int y, Operation op, int index, String possibility) {
+            generatePossibilities(
+                    /* target= */ target,
+                    /* x= */ eval(x, y, op),
+                    /* y= */ getDigitAsInt(index),
+                    /* op= */ Operation.ADDITION,
+                    /* index= */ index + 1,
+                    /* possibility= */ String.format("%s+%d", possibility, getDigitAsInt(index)));
+        }
+
+        void doSubtractionOp(
+                int target, int x, int y, Operation op, int index, String possibility) {
+            generatePossibilities(
+                    /* target= */ target,
+                    /* x= */ eval(x, y, op),
+                    /* y= */ getDigitAsInt(index),
+                    /* op= */ Operation.SUBTRACTION,
+                    /* index= */ index + 1,
+                    /* possibility= */ String.format("%s-%d", possibility, getDigitAsInt(index)));
+        }
+
+        void doMultiplicationOp(
+                int target, int x, int y, Operation op, int index, String possibility) {
+            switch (op) {
+                case MULTIPLICATION:
+                    generatePossibilities(
+                            /* target= */ target,
+                            /* x= */ eval(x, y, Operation.MULTIPLICATION),
+                            /* y= */ getDigitAsInt(index),
+                            /* op= */ Operation.MULTIPLICATION,
+                            /* index= */ index + 1,
+                            /* possibility= */ String.format("%s*%d", possibility, getDigitAsInt(index)));
+                    break;
+                case ADDITION:
+                        generatePossibilities(
+                            /* target= */ target + (x < 0 ? -x : x),
+                            /* x= */ y,
+                            /* y= */ getDigitAsInt(index),
+                            /* op= */ Operation.MULTIPLICATION,
+                            /* index= */ index + 1,
+                            /* possibility= */ String.format("%s*%d", possibility, getDigitAsInt(index)));
+                        break;
+                case SUBTRACTION:
+                        generatePossibilities(
+                            /* target= */ target + (x < 0 ? -x : x),
+                            /* x= */ -y,
+                            /* y= */ getDigitAsInt(index),
+                            /* op= */ Operation.MULTIPLICATION,
+                            /* index= */ index + 1,
+                            /* possibility= */ String.format("%s*%d", possibility, getDigitAsInt(index)));
+                        break;
+                default:
+                        throw new RuntimeException(String.format("Illegal previous operation: %s", op));
+            }
+        }
+
+
         void generatePossibilities(
-                int index, int sum, int factor, String possibility) {
+                int target, int x, int y, Operation op, int index, String possibility) {
+            /* ASSUMPTIONS:
+             * If all numbers have been "consumed" (ie, if you are at the end of `num`),
+             * then you must apply the final operation:
+             *      if eval(x, y, op) == target:
+             *          our possibility is valid.
+             *          we must add it to the list.
+             *          However, one question we must ask ourselves:
+             *          Does `possibility` already include this latest operation?
+             *          We should make it so since this operation is final and it
+             *          keeps things cleaner (we don't inherit debt from previous call).
+             */
             if (index == num.length()) {
-                if (sum + factor == target) {
+                if (eval(x, y, op) == target) {
                     possibilities.add(possibility);
+                }
+                if (Solution.evalExpr(possibility) != eval(x, y, op)) {
+                    throw new RuntimeException(String.format(
+                                "Incorrect Expr!\n" +
+                                "possibility = %s\n" +
+                                "target = %d\n" +
+                                "x = %d\n" +
+                                "y = %d\n" +
+                                "op = %s\n" +
+                                "eval(%d, %d, %s) = %d\n" +
+                                "evalExpr(\"%s\") = %d\n",
+                                possibility,
+                                target,
+                                x,
+                                y,
+                                op,
+                                x,
+                                y,
+                                op,
+                                eval(x, y, op),
+                                possibility,
+                                Solution.evalExpr(possibility)));
                 }
                 return;
             }
-
-            char digitAsChar = getDigitAsChar(index);
             int digitAsInt = getDigitAsInt(index);
-
-            // PLUS(+):
-            generatePossibilities(
-                    index + 1,
-                    sum + factor,
-                    digitAsInt,
-                    String.format("%s+%s", possibility, digitAsChar));
-            // MINUS(-):
-            generatePossibilities(
-                    index + 1,
-                    sum + factor,
-                    -digitAsInt,
-                    String.format("%s-%s", possibility, digitAsChar));
-            // MULTIPLY(*):
-            generatePossibilities(
-                    index + 1,
-                    sum,
-                    factor * digitAsInt,
-                    String.format("%s*%s", possibility, digitAsChar));
-            // APPEND(factor):
-            generatePossibilities(
-                    index + 1,
-                    sum,
-                    factor * 10 + digitAsInt,
-                    String.format("%s%s", possibility, digitAsChar));
-        }
-
-        char getDigitAsChar(int index) {
-            return num.charAt(index);
+            // Now generate strings with (possibly) different operation.
+            doAdditionOp(target, x, y, op, index, possibility);
+            // ADDITION(+):
+            doAdditionOp(target, x, y, op, index, possibility);
+            // SUBTRACTION(-):
+            doSubtractionOp(target, x, y, op, index, possibility);
+            // MULTIPLICATION(*):
+            // This will be more involved.
+            doMultiplicationOp(target, x, y, op, index, possibility);
         }
 
         int getDigitAsInt(int index) {
-            return Character.getNumericValue(getDigitAsChar(index));
+            return Character.getNumericValue(num.charAt(index));
         }
 
-        Solution1(String _num, int _target) {
+        int eval(int x, int y, Operation op) {
+            switch (op) {
+                case MULTIPLICATION:
+                    return x * y;
+                case ADDITION:
+                    return x + y;
+                case SUBTRACTION:
+                    return x - y;
+                case APPEND:
+                    if (x == 0) {
+                        throw new RuntimeException("Cannot append to a leading zero!");
+                    }
+                    if (y < 0) {
+                        throw new RuntimeException(String.format("Cannot append a negative number: %d", y));
+                    }
+                    if (y % 10 > 0) {
+                        throw new RuntimeException(String.format("Appending number must be a single digit: %d", y));
+                    }
+                    return (Math.abs(x) + y) * (x < 0 ? -1 : 1);
+            }
+            throw new RuntimeException(String.format("Unknown Operation: %s", op));
+        }
+
+        Solution1(String _num) {
             num = _num;
-            target = _target;
             possibilities = new ArrayList<>();
         }
     }
--- playground/expressionaddoperators/Tester.java
+++ playground/expressionaddoperators/Tester.java
@@ -1,24 +1,50 @@
 package playground.expressionaddoperators;
 
+import java.lang.Character;
+import java.lang.StringBuilder;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptEngine;
+import javax.script.ScriptException;
 
 // TODO: Add the necessary imports to Solution.java
 
 /********************************************************************
-TO RUN THIS CLASS::
+
+TO RUN THIS CLASS:
 
 $ cd ~/GitHub && \
-javac playground/expressionaddoperators/Tester.java && \
+javac -g playground/expressionaddoperators/Tester.java && \
 java playground.expressionaddoperators.Tester;
+
+
+TO RUN DEBUGGER:
+
+$ cd ~/GitHub && \
+javac -g playground/expressionaddoperators/Tester.java && \
+jdb playground.expressionaddoperators.Tester;
 
 ********************************************************************/
 
-
 public class Tester {
 
-    private Solution solution;
+    private static final Set<Character> VALID_OPERATORS =
+        new HashSet<>(Arrays.asList('+', '-', '*'));
 
+    private Solution solution;
+    private ScriptEngine scriptEngine;
+ 
     Tester() {
          solution = new Solution();
+         ScriptEngineManager scriptEngineManager =
+             new ScriptEngineManager();
+         scriptEngine =
+             scriptEngineManager.getEngineByName("JavaScript");
     }
 
     public static void main(String[] unusedArgs) {
@@ -26,9 +52,151 @@ public class Tester {
          tester.runTests();
     }
 
+    private static class Test {
+
+        private final String num;
+        private final int target;
+        private final List<String> expected;
+
+        private Test(String num, int target, List<String> expected) {
+            this.num = num;
+            this.target = target;
+            this.expected = expected;
+        }
+
+        public static Test create(
+                String num, int target, List<String> expected) {
+            return new Test(num, target, expected);
+        }
+
+        public String getNum() { return num; }
+
+        public int getTarget() { return target; }
+
+        public List<String> getExpected() { return expected; }
+    }
+
     private void runTests() {
-         // TODO: Implement.
-         System.out.println("Initializing all tests ...");
-         System.out.println("Testing complete.");
+         System.out.println("Initializing all tests ...\n");
+
+         List<Test> tests =
+             Arrays.asList(
+                 Test.create("123", 6, Arrays.asList("1*2*3","1+2+3")),
+                 Test.create("232", 8, Arrays.asList("2*3+2","2+3*2")),
+                 Test.create("3456237490", 9191, Arrays.asList()),
+                 Test.create("105", 5, Arrays.asList("1*0+5","10-5")));
+
+         for (Test test : tests) { outputTestResults(test); }
+
+         System.out.println("\nTesting complete.\n");
+    }
+
+    private void outputTestResults(Test test) {
+        // Get actual output.
+        List<String> actual =
+            solution.addOperators(test.getNum(), test.getTarget());
+        // Sort arrays for better visual comparisons.
+        Collections.sort(test.getExpected());
+        Collections.sort(actual);
+        // Output results.
+        System.out.println(String.format(
+                    "INPUT:\t\t\t{ num=\"%s\", target=%d }",
+                    test.getNum(), test.getTarget()));
+        System.out.println(String.format(
+                    "EXPECTED:\t\t%s",
+                    serializePossibilities(test.getExpected())));
+        System.out.println(String.format(
+                    "ACTUAL:\t\t\t%s",
+                    serializePossibilities(actual)));
+        outputIncorrectMathExpressions(
+                test.getTarget(), actual);
+        System.out.println();
+    }
+
+    private static String serializePossibilities(List<String> possibilities) {
+        if (possibilities.isEmpty()) {
+            return "[]";
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+
+        stringBuilder.append("[ ");
+        for (int i = 0; i < possibilities.size(); i++) {
+            stringBuilder.append(String.format("\"%s\"", possibilities.get(i)));
+            if (i < possibilities.size() - 1) {
+                stringBuilder.append(", ");
+            }
+        }
+        stringBuilder.append(" ]");
+
+        return stringBuilder.toString();
+    }
+
+    private void outputIncorrectMathExpressions(
+            int target, List<String> expressions) {
+        expressions
+            .stream()
+            .filter(expr -> {
+                try {
+                    return eval(expr) != target;
+                } catch (ScriptException e) {
+                    throw new RuntimeException(String.format(
+                                "Eval error occurred: expr=\"%s\".\n%s", expr, e));
+                }
+            })
+            .map(expr -> 
+                    String.format(
+                        "INCORRECT_EXPR:\t\t\"%s\"",
+                        expr))
+            .forEach(System.out::println);
+
+    }
+
+    private int eval(String expr) throws ScriptException {
+        validateSimpleMathExpr(expr);
+        return (Integer) scriptEngine.eval(expr);
+    }
+
+    /**
+     * Validates a simple math expression.
+     */
+    private void validateSimpleMathExpr(String expr) {
+        // Input expression must comply with the following requirements:
+        //      1.  Cannot be null (or empty). Otherwise, mathematical evaluation
+        //          of `expr` will be meaningless.
+        //      2.  Must start and end with a series of digits.
+        //          This is because mathematical operators can only exist between
+        //          digits.
+        //      4.  Only digits and allowed mathematical operators are valid.
+        //      3.  Mathematical operators must be followed up with a digit.
+        if (expr == null || expr.isEmpty()) {
+            throw new RuntimeException(String.format(
+                        "Recieved null (or empty) math expression: %s", expr));
+        }
+        final int N = expr.length();
+        char firstChar = expr.charAt(0);
+        char lastChar = expr.charAt(N - 1);
+        if (!Character.isDigit(firstChar)) {
+            throw new RuntimeException(String.format(
+                        "Initial char of math expr must be a digit: %s", expr));
+        }
+        if (!Character.isDigit(lastChar)) {
+            throw new RuntimeException(String.format(
+                        "Final char of math expr must be a digit: %s", expr));
+        }
+        for (int i = 1; i < N - 1; i++) {
+            char currChar = expr.charAt(i);
+            if (Character.isDigit(currChar)) {
+                continue;
+            } 
+            if (!VALID_OPERATORS.contains(currChar)) {
+                throw new RuntimeException(String.format(
+                            "Math expr contains illegal character(s): %s", expr));
+            }
+            char nextChar = expr.charAt(i+1);
+            if (!Character.isDigit(nextChar)) {
+                throw new RuntimeException(String.format(
+                            "Math operator must be followed by a digit: %s", expr));
+            }
+        }
     }
 }
