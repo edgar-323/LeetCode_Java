9eddc8a230bc0f339f0a62431da8a2bf533786d8
Impatch <edgarbarboza@Natalys-MacBook-Pro.local>
2022-03-24 20:51:59 -0700

    tough

---
+++ exercises/BinaryTreeMaximumPathSum/Solution.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxPathSum(TreeNode root) {
+        return Solution1.maxPathSum(root);
+    }
+
+    static class Solution1 {
+        static int maxPathSum(TreeNode root) {
+            throw new RuntimeException("Not implemented");
+        }
+    }
+}
