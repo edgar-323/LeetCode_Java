5eee15ca1f807cf257cfc70923b4e71ad0027c3a
Impatch <edgarbarboza@Natalys-MacBook-Pro.local>
2022-03-24 20:40:47 -0700

    PathSum

--- exercises/PathSum/Solution.java
+++ exercises/PathSum/Solution.java
@@ -15,12 +15,61 @@
  */
 class Solution {
     public boolean hasPathSum(TreeNode root, int targetSum) {
-        return Solution1(root, targetSum);
+        return Solution2.hasPathSum(root, targetSum);
     }
 
     static class Solution1 {
-        static boolean hasPathSum(TreeNode root, int targetSum) {
-            throw new RuntimeException("Not implemented");
+        /**
+         * TimeComplexity:  O(N)
+         * SpaceComplexity: O(N) for a linear tree but can be O(log(N)) for a balanced tree.
+         *
+         * Where:
+         *      N = number of nodes in root.
+         */
+        static boolean hasPathSum(TreeNode node, int targetSum) {
+            if (node == null) {
+                return false;
+            }
+            if (node.left == null && node.right == null) {
+                return node.val == targetSum;
+            }
+            return hasPathSum(node.left, targetSum - node.val)
+                || hasPathSum(node.right, targetSum - node.val);
         }
-    }    
+    }
+
+    static class Solution2 {
+        /**
+         * TimeComplexity:  O(N)
+         * SpaceComplexity: O(N) for a very short tree but can be O(1) for a linear tree.
+         *
+         * Where:
+         *      N = number of nodes in root.
+         */
+        static boolean hasPathSum(TreeNode root, int targetSum) {
+            if (root == null) {
+                return false;
+            }
+
+            Queue<TreeNode> Q = new LinkedList<>();
+            Q.add(root);
+
+            while (!Q.isEmpty()) {
+                TreeNode node = Q.poll();
+                if (node.left == null && node.right == null && node.val == targetSum) {
+                    return true;
+                }
+                if (node.left != null) {
+                    node.left.val += node.val;
+                    Q.add(node.left);
+                }
+                if (node.right != null) {
+                    node.right.val += node.val;
+                    Q.add(node.right);
+                }
+            }
+
+            return false;
+        }
+    }
 }
