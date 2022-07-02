cfb3919bf0dbedf2225ffabcc6ae3502f5160c08
Impatch <edgarbarboza@Natalys-MacBook-Pro.local>
2022-07-01 21:09:28 -0700

    another problem

---
+++ exercises/MaximumSubarray/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        return Solution1.maxSubArray(nums);
+    }
+
+    private static class Solution1 {
+        static int maxSubArray(int[] nums) {
+            return -1;
+        }
+    }
+}
