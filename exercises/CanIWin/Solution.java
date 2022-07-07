b83b7cc9177424fe133090848d25b22ad32777b2
Impatch <edgarbarboza@Natalys-MacBook-Pro.local>
2022-07-07 00:25:16 -0700

    CanIWin

---
+++ exercises/CanIWin/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean canIWin(int maxChoosableInteger, int desiredTotal) {
+        return Solution1.canIWin(maxChoosableInteger, desiredTotal);
+    }
+
+    private static class Solution1 {
+        static boolean canIWin(int maxChoosableInteger, int desiredTotal) {
+            // TODO(edgarnunez): Implement.
+            return false;
+        }
+    }
+}
