24268b05a7e26200bb144dc4dfbe1c733fb20f1d
Impatch <edgarbarboza@Natalys-MacBook-Pro.local>
2022-07-10 22:18:23 -0700

    MaximumProductSubarray

---
+++ exercises/MaximumProductSubarray/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        return Solution1.maxProduct(nums);
+    }
+
+    static private class Solution1 {
+        static int maxProduct(int[] nums) {
+            throw new RuntimeException("NOT IMPLEMENTED");
+        }
+    }
+}
