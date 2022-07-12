fe7c5f39a67643ef89a0a1b644aa8ab7bb0bbd05
Impatch <edgarbarboza@Natalys-MacBook-Pro.local>
2022-07-11 22:44:21 -0700

    new prob

---
+++ exercises/WordBreak/Solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        return Solution1.wordBreak(s, wordDict);
+    }
+
+    private static class Solution1 {
+        static boolean wordBreak(String s, List<String> wordDict) {
+        }
+    }
+}
