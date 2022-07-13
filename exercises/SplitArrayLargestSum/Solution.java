10a5eb224ec12090c88b16034d3e869ab85ca1dc
Impatch <edgarbarboza@Natalys-MacBook-Pro.local>
2022-07-12 19:53:20 -0700

    Starting "410. Split Array Largest Sum"

---
+++ exercises/SplitArrayLargestSum/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int splitArray(int[] nums, int m) {
+        return Solution1.splitArray(nums, m);
+    }
+
+    private static class Solution1 {
+        static int splitArray(int[] nums, int m) {
+            throw new RuntimeException("UNIMPLEMENTED");
+        }
+    }
+}
