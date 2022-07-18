d9eda4720dae40f200f0fd7489bbf06a97459334
Impatch <edgarbarboza@Natalys-MacBook-Pro.local>
2022-07-17 21:34:07 -0700

    update

---
+++ exercises/AllocateMailboxes/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minDistance(int[] houses, int k) {
+        return Solution1.minDistance(houses, k);
+    }
+
+    private static class Solution1 {
+        static int minDistance(int[] houses, int k) {
+            throw new RuntimeException("Solution1: NOT IMPLEMENTED");
+        }
+    }
+}
