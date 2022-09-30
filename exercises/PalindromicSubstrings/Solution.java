3b6fb73b44286efd637987d1cb8817dfa5e8b93a
Impatch <edgarbarboza@Natalys-MacBook-Pro.local>
2022-09-29 23:05:16 -0700

    msg

---
+++ exercises/PalindromicSubstrings/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int countSubstrings(String s) {
+        return Solution1.countSubstrings(s);
+    }
+
+    private static class Solution1 {
+        static countSubstrings(String s) {
+            return -1;
+        }
+    }
+}
