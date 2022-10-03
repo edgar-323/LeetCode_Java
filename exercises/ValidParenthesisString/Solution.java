8f3ca8f0b21a0d2c11567b4ea48dc47232e85506
Impatch <edgarbarboza@Natalys-MacBook-Pro-2.local>
2022-10-02 21:08:06 -0700

    completed #678

--- exercises/ValidParenthesisString/Solution.java
+++ exercises/ValidParenthesisString/Solution.java
@@ -3,9 +3,85 @@ class Solution {
         return Solution1.checkValidString(s);
     }
 
+    /**
+     * TimeComplexity:  O(N^2)
+     * SpaceComplexity: O(N^2)
+     *
+     * Where:
+     *      N = s.length()
+     */
     private static class Solution1 {
+
+        private final String s;
+        private final int N;
+        private final Boolean[][] cache;
+
+
         static boolean checkValidString(String s) {
-            throw new RuntimeException("NOT IMPLEMENTED");
+            return createSolver(s).isValid(/* index= */ 0, /* unpairedOpenParenthesis= */ 0);
+        }
+
+        private boolean isValid(int i, int unpairedOpenParenthesis) {
+            if (i == N) {
+                return unpairedOpenParenthesis == 0;
+            }
+            if (inCache(i, unpairedOpenParenthesis)) {
+                return getCacheValue(i, unpairedOpenParenthesis);
+            }
+
+            boolean result;
+            if (s.charAt(i) == '(') {
+                result = insertOpenParenthesis(i, unpairedOpenParenthesis);
+            } else if (s.charAt(i) == ')') {
+                result = insertClosedParenthesis(i, unpairedOpenParenthesis);
+            } else { // s.charAt(i) == '*'
+                result =
+                    insertOpenParenthesis(i, unpairedOpenParenthesis)
+                    || insertClosedParenthesis(i, unpairedOpenParenthesis)
+                    || insertNoParenthesis(i, unpairedOpenParenthesis);
+            }
+
+            return setCacheValue(i, unpairedOpenParenthesis, result);
+        }
+
+        private boolean insertOpenParenthesis(int i, int unpairedOpenParenthesis) {
+            return isValid(i + 1, unpairedOpenParenthesis + 1);
+        }
+
+        private boolean insertClosedParenthesis(int i, int unpairedOpenParenthesis) {
+            return unpairedOpenParenthesis > 0 && isValid (i + 1, unpairedOpenParenthesis - 1);
+        }
+
+        private boolean insertNoParenthesis(int i, int unpairedOpenParenthesis) {
+            return isValid(i + 1, unpairedOpenParenthesis);
+        }
+
+        private boolean inCache(int i, int unpairedOpenParenthesis) {
+            return cache[i][unpairedOpenParenthesis] != null;
+        }
+
+        private boolean getCacheValue(int i, int unpairedOpenParenthesis) {
+            return cache[i][unpairedOpenParenthesis];
+        }
+
+        private boolean setCacheValue(int i, int unpairedOpenParenthesis, boolean value) {
+            cache[i][unpairedOpenParenthesis] = value;
+            return value;
+        }
+
+        private static Solution1 createSolver(String s) {
+            return new Solution1(s);
+        }
+
+        private Solution1(String s) {
+            this.s = s;
+            N = s.length();
+            cache = new Boolean[N][N+1];
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j <= N; j++) {
+                    cache[i][j] = null;
+                }
+            }
         }
     }
 }
