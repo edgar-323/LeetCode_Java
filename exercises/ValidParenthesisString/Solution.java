8d4942ffe9ea912299d8c0ed320301300ac7d7ee
Impatch <edgarbarboza@Natalys-MacBook-Pro-2.local>
2022-10-02 17:56:55 -0700

    adding 678

---
+++ exercises/ValidParenthesisString/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        return Solution1.checkValidString(s);
+    }
+
+    private static class Solution1 {
+        static boolean checkValidString(String s) {
+            throw new RuntimeException("NOT IMPLEMENTED");
+        }
+    }
+}
