4f28602adfffbc7ed20186bb3831189d04028f66
Impatch <edgarbarboza@Natalys-MacBook-Pro-2.local>
2022-10-04 19:56:50 -0700

    Adding #787

---
+++ exercises/CheapestFlightsWithinKStops/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        return Solution.findCheapestPrice(n, flights, src, dst, k);
+    }
+
+    private static class Solution1 {
+        static int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+            throw new RuntimeException("Solution1 Not Implemented!")
+        }
+    }
+}
