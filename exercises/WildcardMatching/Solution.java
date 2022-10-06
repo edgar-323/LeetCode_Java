587a5e3b5dd852a97f27b09a8a12384230502362
Impatch <edgarbarboza@Natalys-MacBook-Pro-2.local>
2022-10-05 22:08:45 -0700

    Adding #44

---
+++ exercises/WildcardMatching/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        return Solution1.isMatch(s, p);
+    }
+
+    private static class Solution1 {
+        static boolean isMatch(String s, String p) {
+            throw new RuntimeException("NOT IMPLEMENTED");
+        }
+    }
+}
