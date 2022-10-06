cfc8840ca321e4b12d6e96f426b1337528fab031
Impatch <edgarbarboza@Natalys-MacBook-Pro-2.local>
2022-10-06 00:10:40 -0700

    Adding #72

---
+++ exercises/EditDistance/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minDistance(String word1, String word2) {
+        return Solution1.minDistance(word1, word2);
+    }
+
+    private static class Solution1 {
+        static int minDistance(String word1, String word2) {
+            throw new RuntimeException("NOT IMPLEMENTED");
+        }
+    }
+}
