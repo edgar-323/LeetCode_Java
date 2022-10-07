01db4165a320901163da1d535309f657189a64eb
Impatch <edgarbarboza@Natalys-MacBook-Pro-2.local>
2022-10-06 22:31:39 -0700

    Finished #72

--- exercises/EditDistance/Solution.java
+++ exercises/EditDistance/Solution.java
@@ -3,7 +3,19 @@ class Solution {
         return Solution1.minDistance(word1, word2);
     }
 
+    /**
+     * TimeComplexity:  O(M*N)
+     * SpaceComplexity: O(M*N)
+     */
     private static class Solution1 {
+        private static int INSERT_COST = 1;
+        private static int DELETE_COST = 1;
+        private static int REPLACE_COST = 1;
+
+        private final int M,N;
+        private final String word1, word2;
+        private final Integer[][] cache;
+        
         // Thoughts:
         // Let's work with word1[i:] and word2[j:].
         // Define:
@@ -14,7 +26,7 @@ class Solution {
         //  1. Insert word2[j] BEFORE word1[i]
         //     In this case, we now have to still account for word1[i:] and word2[(j+1):].
         //  2. Delete word1[i].
-        //     Hmm, we no have to account for word1[(i+1):] and word2[(j+1):]
+        //     Hmm, we no have to account for word1[(i+1):] and word2[j:]
         //  3. Replace word1[i] with word2[j].
         //     We have to account for word1[(i+1):] and word2[(j+1):]
         // In the three above cases we must add 1 to each case since each operation has
@@ -34,20 +46,73 @@ class Solution {
         //     We only have option to DELETE word1[i] and then have to account for
         //     word1[(i+1):] and word2[j:]
 
-        private final String word1;
-        private final String word2;
-
         static int minDistance(String word1, String word2) {
-            throw new RuntimeException("NOT IMPLEMENTED");
+            return newSolver(word1, word2).solve();
+        }
+
+        private int solve() {
+            return solve(0, 0);
+        }
+
+        private int solve(int i, int j) {
+            if (cacheHit(i, j)) {
+                return getCache(i, j);
+            }
+
+            int minCost;
+            if (i == M && j == N) {
+                minCost = 0;
+            } else if (i == M) {
+                minCost = solve(i, j+1) + INSERT_COST;
+            } else if (j == N) {
+                minCost = solve(i+1, j) + DELETE_COST;
+            } else {
+                minCost = min(
+                        solve(i, j+1)   + INSERT_COST,
+                        solve(i+1, j)   + DELETE_COST,
+                        solve(i+1, j+1) + replaceCost(i, j));
+            }
+
+            return setCache(i, j, minCost);
         }
 
         private static Solution1 newSolver(String word1, String word2) {
             return new Solution1(word1, word2);
         }
 
+        private boolean cacheHit(int i, int j) {
+            return cache[i][j] != null;
+        }
+
+        private int getCache(int i, int j) {
+            return cache[i][j];
+        }
+
+        private int setCache(int i, int j, int v) {
+            cache[i][j] = v;
+            return v;
+        }
+
+        private int replaceCost(int i, int j) {
+            return word1.charAt(i) == word2.charAt(j) ? 0 : REPLACE_COST;
+        }
+
+        private static int min(int a, int b, int c) {
+            return Math.min(a, Math.min(b, c));
+        }
+
         private Solution1(String word1, String word2) {
+            this.M = word1.length();
+            this.N = word2.length();
             this.word1 = word1;
             this.word2 = word2;
+
+            cache = new Integer[M+1][N+1];
+            for (int i = 0; i <= M; i++) {
+                for (int j = 0; j <= N; j++) {
+                    cache[i][j] = null;
+                }
+            }
         }
     }
 }
