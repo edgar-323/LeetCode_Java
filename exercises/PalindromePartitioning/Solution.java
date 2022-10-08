8e1888e89b584c15dacbe1859746450579296338
Impatch <edgarbarboza@Natalys-MacBook-Pro-2.local>
2022-10-08 14:32:54 -0700

    Small improvements

--- exercises/PalindromePartitioning/Solution.java
+++ exercises/PalindromePartitioning/Solution.java
@@ -30,7 +30,6 @@ class Solution {
         //      convenient that LinkedList<T> implements the
         //      interface List<T>.
 
-        private final int N;
         private final String s;
         private final List<String>[] palindromes;
         private final Map<Integer, List<List<String>>> cache;
@@ -79,19 +78,26 @@ class Solution {
          */
         Solution1(String s) {
             this.s = s;
-            this.N = s.length();
-            cache = new HashMap<>();
-            List<List<String>> empty = new ArrayList<>();
-            empty.add(new ArrayList<>());
-            cache.put(N, empty);
+            int N = s.length();
+
+            // In the base case (i == N), we will return a list of
+            // a single empty partition (also an empty list).
+            // This is because there is no palindomes in the empty
+            // string slice: S[N:].
+            cache = new HashMap<>() {{
+                put(N, new ArrayList<>() {{
+                    add(new ArrayList<>());
+                }});
+            }};
 
             palindromes = new ArrayList[N];
             for (int i = 0; i < N; i++) {
                 palindromes[i] = new ArrayList<>();
             }
 
+            // isPalindrome[i][j] will help us compute palindromes[i].
             boolean[][] isPalindrome = new boolean[N][N];
-
+            // Compute palindromes of lengths: {1, 2}.
             for (int i = 0; i < N; i++) {
                 isPalindrome[i][i] = true;
                 palindromes[i].add(s.substring(i, i+1));
@@ -102,13 +108,14 @@ class Solution {
                     }
                 }
             }
+            // Compute palindromes of lengths: {3, 4, ..., N}.
             for (int subarraySize = 3; subarraySize <= N; subarraySize++) {
                 for (int i = 0; (i + subarraySize) <= N; i++) {
-                    int j = i + subarraySize;
-                    isPalindrome[i][j-1] =
-                        isPalindrome[i+1][j-2] && (s.charAt(i) == s.charAt(j - 1));
-                    if (isPalindrome[i][j-1]) {
-                        palindromes[i].add(s.substring(i, j));
+                    int j = i + subarraySize - 1;
+                    isPalindrome[i][j] =
+                        isPalindrome[i+1][j-1] && (s.charAt(i) == s.charAt(j));
+                    if (isPalindrome[i][j]) {
+                        palindromes[i].add(s.substring(i, j+1));
                     }
                 }
             }
