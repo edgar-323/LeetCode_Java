b6f193613ed59c35ffa443fbf67b960da31ed985
Impatch <edgarbarboza@Natalys-MacBook-Pro-2.local>
2022-10-07 19:14:32 -0700

    Adding #131

---
+++ exercises/PalindromePartitioning/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public List<List<String>> partition(String s) {
+        return Solution1.partition(s);
+    }
+
+    static class Solution1 {
+        static List<List<String>> partition(String s) {
+            throw new RuntimeException("NOT_IMPLEMENTED");
+        }
+    }
+}
