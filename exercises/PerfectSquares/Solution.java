a4f9162b5fe0b272ce3d4591dce8826ee6174f15
Impatch <edgarbarboza@Natalys-MacBook-Pro-2.local>
2022-10-10 23:17:54 -0700

    Finished #279

--- exercises/PerfectSquares/Solution.java
+++ exercises/PerfectSquares/Solution.java
@@ -3,19 +3,39 @@ class Solution {
         return Solution1.newSolver(n).solve();
     }
 
+    /**
+     * TimeComplexity:  O(N * sqrt(N))
+     * SpaceComplexity: O(N)
+     */
     static class Solution1 {
         private final int N;
+        private final Map<Integer, Integer> cache;
 
-        static int newSolver(int n) {
+        static Solution1 newSolver(int n) {
             return new Solution1(n);
         }
 
         int solve() {
-            throw new RuntimeException("NOT IMPLEMENTED");
+            return solve(N);
+        }
+
+        int solve(int n) {
+            if (cache.containsKey(n)) {
+                return cache.get(n);
+            }
+
+            int minSquares = Integer.MAX_VALUE;
+            for (int i = (int) Math.sqrt(n); i >= 1; i--) {
+                minSquares = Math.min(minSquares, 1 + solve(n - i*i));
+            }
+
+            cache.put(n, minSquares);
+            return minSquares;
         }
 
         Solution1(int n) {
             N = n;
+            cache = new HashMap<>() {{ put(0, 0); }};
         }
     }
 }
