afd56c6749347ac5b9269ae19ec659afd8b069b9
Impatch <edgarbarboza@Natalys-MacBook-Pro-2.local>
2022-11-30 00:21:59 -0800

    Finally completed ExpressionAddOperators

--- exercises/ExpressionAddOperators/Solution.java
+++ exercises/ExpressionAddOperators/Solution.java
@@ -1,124 +1,166 @@
 class Solution {
     public List<String> addOperators(String num, int target) {
-        return Solution1.addOperators(num, target);
+        return Solution1.newSolver(num, target).solve();
     }
 
-    private static class Solution1 {
-        // Naive (1st attempt) implementation.
-        // Possible operators: {+, -, *}.
-        // Say you have the sequence (string) of digits:
-        //      d_0, d_1, ..., d_(n-1).
-        // Then you only assign an operator to all except the first digit (d_0).
-        //
-        // at any given time (ie, when you are looking at digit d_i), you have a
-        // current_sum and a current_factor.
-        // What options do we have with d_i ?
-        //
-        //   PLUS(+):
-        //      (current_sum + current_factor, + d_i)
-        //      --> Rationale: as soon as you assign a non-multiplier op, you
-        //          "break" the factor stream (and thus it "ends" -- must be
-        //          added to current_sum).
-        //  MINUS(-):
-        //      (current_sum + current_factor, - d_i)
-        //      --> Rationale: Same as the PLUS rationale.
-        //  MULTIPLY(*):
-        //      (current_sum, current_factor * d_i)
-        //      --> Rationale: You are extending the multiplication stream.
-        //
-        // At each step, you consume the digit (ie, increment the index).
-        //
-        // At d_0, there is no concept of (current_sum, current_factor).
-        // Well, actually, there is: (0, 1)
-        // However, you cannot assign the MINUS(-) operator at d_0.
-        // So at d_0, you can only generate: {(d_0, 1), (0, +d_0)}
-        //
-        // At index N, there are no more digits, which indicates that you must
-        // consolidate the existing tuple:
-        //      (current_sum, current_factor) --> current_sum + current_factor
-        // Then, compare this value with `target`:
-        //      if (i == N && current_sum + current_factor == target) {
-        //          possibilities.add(current_sting);
-        //          return;
-        //      }
-        //
-        // We've been ignoring the construction of the final string:
-        //      OP: (current_sting + "${OP}" + d_i), for OP in {+, -, *}.
-        //
 
-        private final String num;
+    static class ValueAndIndex {
+        private final long value;
+        private final int index;
+
+        ValueAndIndex(long value, int index) {
+            this.value = value;
+            this.index = index;
+        }
+
+        long getValue() {
+            return value;
+        }
+
+        int getIndex() {
+            return index;
+        }
+
+        static ValueAndIndex create(long value, int index) {
+            return new ValueAndIndex(value, index);
+        }
+    }
+
+    static class Solution1 {
+        // THIS IS MY MOST NAIVE ATTEMPT (BRUTE FORCE!).
+
+        private static final char ADDITION = '+';
+        private static final char SUBTRACTION = '-';
+        private static final char MULTIPLICATION = '*';
+        private static final char CONCAT = '#';
+        private static final char[] OPS = {
+            ADDITION,
+            SUBTRACTION,
+            MULTIPLICATION,
+            CONCAT
+        };
+
+        private final int N;
         private final int target;
-        private final List<String> possibilities;
+        private final char[] combo;
 
-        static List<String> addOperators(String num, int target) {
-            return new Solution1(num, target).generatePossibilities();
+        static Solution1 newSolver(String num, int target) {
+            return new Solution1(num, target);
         }
 
-        List<String> generatePossibilities() {
-            // Initial states:
-            int initialIndex = 0; 
-            char digitAsChar = getDigitAsChar(0);
-            int digitAsInt = getDigitAsInt(0);
+        List<String> solve() {
+            List<String> combinations = new ArrayList<>();
+            solve(0, toDigit(0) == 0, combinations);
 
-            generatePossibilities(
-                    /* index= */ 1,
-                    /* sum= */ 0,
-                    /* factor= */ digitAsInt,
-                    /* possibility= */ Character.toString(digitAsChar));
-
-            return possibilities;
+            return combinations;
         }
 
-        void generatePossibilities(
-                int index, int sum, int factor, String possibility) {
-            if (index == num.length()) {
-                if (sum + factor == target) {
-                    possibilities.add(possibility);
+        /**
+         * Assumption: combo[i] is a digit that belongs to the original `num` string.
+         * Objective: This method attempts to place an operator in front of the i-th
+         *            digit. It will avoid placing the concatenation operator if the
+         *            i-th digit is a zero. Furthermore, if there are no more digits
+         *            after the i-th digit, then we must stop this assignment, which
+         *            also means that we will evaluate the value of the current combo
+         *            and if it matches our target, we will add it to our solution
+         *            combinations.
+         */
+        void solve(int i, boolean isLeadingZero, List<String> combinations) {
+            if (i >= (N-1)) {
+                if (evalCombo() == target) {
+                    combinations.add(comboToString());
                 }
                 return;
             }
 
-            char digitAsChar = getDigitAsChar(index);
-            int digitAsInt = getDigitAsInt(index);
-
-            // PLUS(+):
-            generatePossibilities(
-                    index + 1,
-                    sum + factor,
-                    digitAsInt,
-                    String.format("%s+%s", possibility, digitAsChar));
-            // MINUS(-):
-            generatePossibilities(
-                    index + 1,
-                    sum + factor,
-                    -digitAsInt,
-                    String.format("%s-%s", possibility, digitAsChar));
-            // MULTIPLY(*):
-            generatePossibilities(
-                    index + 1,
-                    sum,
-                    factor * digitAsInt,
-                    String.format("%s*%s", possibility, digitAsChar));
-            // APPEND(factor):
-            generatePossibilities(
-                    index + 1,
-                    sum,
-                    factor * 10 + digitAsInt,
-                    String.format("%s%s", possibility, digitAsChar));
+            for (char op : OPS) {
+                if (isLeadingZero && op == CONCAT) {
+                    continue;
+                }
+                combo[i+1] = op;
+                solve(i+2, (op != CONCAT) && (toDigit(i+2) == 0), combinations);
+            }
         }
 
-        char getDigitAsChar(int index) {
-            return num.charAt(index);
+        // Convert the current combo to a string (we just need to strip out the
+        // CONCAT operator occurrences).
+        String comboToString() {
+            int numConcats = 0;
+            for (char c : combo) {
+                if (c == CONCAT) {
+                    numConcats++;
+                }
+            }
+            StringBuilder strBuilder = new StringBuilder(N - numConcats);
+            for (char c : combo) {
+                if (c != CONCAT) {
+                    strBuilder.append(c);
+                }
+            }
+
+            return strBuilder.toString();
         }
 
-        int getDigitAsInt(int index) {
-            return Character.getNumericValue(getDigitAsChar(index));
+        // This method evaluates the value of the current combo.
+        // At this level, we group blocks by multiplication, and
+        // we then apply ADDITION and SUBTRACTION operators over
+        // these blocks; this is because the multiplication blocks
+        // have precedence over the addition/substraction operators.
+        long evalCombo() {
+            ValueAndIndex valueAndIndex = evalMultiplication(0);
+            long value = valueAndIndex.getValue();
+            int i = valueAndIndex.getIndex();
+
+            while ((i+1) < N) {
+                char op = combo[i+1];
+                valueAndIndex = evalMultiplication(i+2);
+                value += (op == SUBTRACTION ? -1 : 1)*valueAndIndex.getValue();
+                i = valueAndIndex.getIndex();
+            }
+
+            return value;
         }
 
-        Solution1(String _num, int _target) {
-            num = _num;
-            target = _target;
-            possibilities = new ArrayList<>();
+        // This method groups blocks by the CONCAT operator and performs
+        // multiplication operations on them.
+        ValueAndIndex evalMultiplication(int i) {
+            ValueAndIndex valueAndIndex = evalConcat(i);
+            long value = valueAndIndex.getValue();
+            i = valueAndIndex.getIndex();
+
+            while ((i+1) < N && combo[i+1] == MULTIPLICATION) {
+                valueAndIndex = evalConcat(i+2);
+                value *= valueAndIndex.getValue();
+                i = valueAndIndex.getIndex();
+            }
+
+            return ValueAndIndex.create(value, i);
+        }
+
+        // This method forms a single group via the CONCAT operator.
+        ValueAndIndex evalConcat(int i) {
+            long value = toDigit(i);
+            while ((i+1) < N && combo[i+1] == CONCAT) {
+                value = 10*value + toDigit(i+2);
+                i += 2;
+            }
+
+            return ValueAndIndex.create(value, i);
+        }
+
+        long toDigit(int i) {
+            return combo[i] - '0';
+        }
+
+        Solution1(String num, int target) {
+            N = 2*num.length() - 1;
+            combo = new char[N];
+
+            for (int j = 0; j < N; j += 2) {
+                combo[j] = num.charAt(j/2);
+            }
+
+            this.target = target;
         }
     }
 }
--- exercises/README.md
+++ exercises/README.md
@@ -5,7 +5,6 @@ Individual problems will be hosted in this directory.
 # Unsolved Exercises
 
 **Outstanding debt:**
-* [**`ExpressionAddOperators`**](./ExpressionAddOperators/)
 * [**`MaximumFrequencyStack`**](./MaximumFrequencyStack/)
 * [**`BinaryTreeMaximumPathSum`**](./BinaryTreeMaximumPathSum/)
 * [**`TopKFrequentElements`**](./TopKFrequentElements/)
