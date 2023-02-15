5b4ff7ec72e7501d5d49932e800c7334396f3c8b
Impatch <edgarbarboza@Natalys-MacBook-Pro-2.local>
2023-02-14 21:55:04 -0800

    Adding #174

---
+++ exercises/DungeonGame/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int calculateMinimumHP(int[][] dungeon) {
+        return Solution1.solve(dungeon);
+    }
+
+    static class Solution1 {
+        static int solve(int[][] dungeon) {
+            throw new RuntimeException("NOT_IMPLEMENTED");
+        }
+    }
+}
