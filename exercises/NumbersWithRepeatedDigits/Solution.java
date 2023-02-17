82406138e3f22cd7150c09e8fce1326654b34055
Impatch <edgarbarboza@Natalys-MacBook-Pro-2.local>
2023-02-16 21:33:25 -0800

    Adding #1012

---
+++ exercises/NumbersWithRepeatedDigits/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int numDupDigitsAtMostN(int n) {
+        return Solution1.solve(n);
+    }
+
+    static class Solution1 {
+        static int solve(int N) {
+            throw new RuntimeException("NOT_IMPLEMENTED");
+        }
+    }
+}
